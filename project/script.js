471b0ddf70514a95ae47d347d6ded814c85a4d5c
tazoor <162966752+tazoor@users.noreply.github.com>
2024-03-18 17:06:45 +0300

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskinput = document.querySelector("#taskInput");
+    const addTaskBtn = document.querySelector("#addTaskBtn");
+    const taskList = document.querySelector("#taskList");
+    const importantTaskList = document.querySelector("#importantTaskList");
+    addTaskBtn.addEventListener('click', () => {
+        taskinput.value.trim() !== "" &&
+            (() => {
+                const newTask = createTaskElement(taskInput.value, moveToImportant);
+                taskList.appendChild(newTask);
+            })();
+        taskInput.value = "";
+});
+
+
+const createTaskElement = (text, moveHandler) => {
+    const newTask = document.createElement("li");
+    const taskText = document.createTextNode(text);
+    const deleteButton = createButton("Удaлить", () => deleteTask(newTask));
+    const moveToImportantButton = createButton("Baжно!", () => moveHandler(newTask));
+    newTask.appendChild(taskText);
+    newTask.appendChild(deleteButton);
+    newTask.appendChild(moveToImportantButton);
+    newTask.addEventListener('click', () => {
+        newTask.classList.toggle('completed');
+    });
+    return newTask;
+};
+const createButton = (text, clickHandler) => { 
+    const button = document.createElement("button");
+    button.appendChild(document.createTextNode(text));
+    button.addEventListener('click', clickHandler);
+    return button;
+    };
+
+    const moveToImportant = (task) => {
+        const importantTask = createTaskElement(task.firstChild.nodeValue, deleteTask); 
+        importantTaskList.appendChild(importantTask);
+        taskList.removeChild(task);
+    };
+
+    const deleteTask = (task) => {
+        task.parentNode.removeChild(task);
+    };
+});
